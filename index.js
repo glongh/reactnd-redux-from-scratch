d409ff3dbb5c427d54e50d7dcf3d55c7b88848a8
glongh <glongh@gmail.com>
2018-05-10 19:34:47 -0400

    Using Action creators

--- index.js
+++ index.js
@@ -30,11 +30,48 @@ function createStore (reducer) {
     }
 }
 
+// Action type constans
 const ADD_TODO = 'ADD_TODO'
 const REMOVE_TODO = 'REMOVE_TODO'
 const TOGGLE_TODO = 'TOGGLE_TODO'
 const ADD_GOAL = 'ADD_GOAL'
 const REMOVE_GOAL = 'REMOVE_GOAL'
+
+// Action creators
+function addTodoAction(todo) {
+    return {
+        type: ADD_TODO,
+        todo
+    }
+}
+
+function removeTodoAction(id) {
+    return {
+        type: REMOVE_TODO,
+        id
+    }
+}
+
+function toggleTodoAction(id) {
+    return {
+        type: TOGGLE_TODO,
+        id
+    }
+}
+
+function addGoalAction(goal) {
+    return {
+        type: ADD_GOAL,
+        goal
+    }
+}
+
+function removeTodoAction(id) {
+    return {
+        type: REMOVE_GOAL,
+        id
+    }
+}
 
 function todos (state = [], action) {
     switch(action.type) {
@@ -75,69 +112,42 @@ store.subscribe(()=>{
 })
 
 // Tests
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
-        id: 0,
-        name: 'Read a book',
-        complete: false
-    }
-})
+store.dispatch(addTodoAction({
+    id: 0,
+    name: 'Read a book',
+    complete: false
+}))
 
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
-      id: 0,
-      name: 'Walk the dog',
-      complete: false,
-    }
-  })
+store.dispatch(addTodoAction({
+    id: 10,
+    name: 'Walk the dog',
+    complete: false,
+}))
   
-  store.dispatch({
-    type: ADD_TODO,
-    todo: {
-      id: 1,
-      name: 'Wash the car',
-      complete: false,
-    }
-  })
+store.dispatch(addTodoAction({
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+}))
   
-  store.dispatch({
-    type: ADD_TODO,
-    todo: {
-      id: 2,
-      name: 'Go to the gym',
-      complete: true,
-    }
-  })
+store.dispatch(addTodoAction({
+    id: 2,
+    name: 'Go to the gym',
+    complete: true
+}))
   
-  store.dispatch({
-    type: REMOVE_TODO,
-    id: 1
-  })
+store.dispatch(removeTodoAction(1))
   
-  store.dispatch({
-    type: TOGGLE_TODO,
-    id: 0
-  })
+store.dispatch(toggleTodoAction(0))
   
-  store.dispatch({
-    type: ADD_GOAL,
-    goal: {
-      id: 0,
-      name: 'Learn Redux'
-    }
-  })
+store.dispatch(addGoalAction({
+    id: 0,
+    name: 'Learn Redux'
+}))
+
+store.dispatch(addGoalAction({
+    id: 1,
+    name: 'Lose 20 pounds'
+}))
   
-  store.dispatch({
-    type: ADD_GOAL,
-    goal: {
-      id: 1,
-      name: 'Lose 20 pounds'
-    }
-  })
-  
-  store.dispatch({
-    type: REMOVE_GOAL,
-    id: 0
-  })
+store.dispatch(removeGoalAction(0))
